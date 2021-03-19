92365ec5d8ff5c3a198dcf06154f9cf90a4cb09e
Dharani196 <69566463+Dharani196@users.noreply.github.com>
2021-03-19 15:29:21 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,12 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lab1
+namespace lab2
 {
-    class Program
-    {
-        static void Main(string[] args)
+    class Programm
+    { }
+
+namespace oops
+    { 
+        public interface xyz
         {
+            void methodA();
+            void methodB();
+        }
+
+          
+        class test : xyz
+        {
+            public void methodA()
+            {
+                Console.WriteLine("methodA");
+            }
+            public void methodB()
+            {
+                Console.WriteLine("methodB");
+            }
+        }    
+        
         }
     }
-}
+
